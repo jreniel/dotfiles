804bd03de8420c5b8356a59f76d1f22e7cf9acd9
Jaime R Calzada <jreniel@gmail.com>
2024-10-05 14:35:26 -0400

    changed plugins

--- .config/nvim/lua/jrcalzada/core/options.lua
+++ .config/nvim/lua/jrcalzada/core/options.lua
@@ -38,17 +38,20 @@ opt.splitright = true
 --
 --
 local function isempty(s)
-    return s == nil or s == ""
+	return s == nil or s == ""
 end
 local function use_if_defined(val, fallback)
-    return val ~= nil and val or fallback
+	return val ~= nil and val or fallback
 end
 
 local conda_prefix = os.getenv("CONDA_PREFIX")
 if not isempty(conda_prefix) then
-    vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
-    vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
 else
-    vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
-    vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
 end
+
+-- ensure :E maps to :Explore
+vim.api.nvim_create_user_command("E", "Explore", {})
--- .config/nvim/lua/jrcalzada/plugins/vimtex.lua
+++ .config/nvim/lua/jrcalzada/plugins/vimtex.lua
@@ -2,5 +2,15 @@ return {
 	"lervag/vimtex",
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_compiler_latexmk = {
+			options = {
+				"-pdf",
+				"-shell-escape",
+				"-verbose",
+				"-file-line-error",
+				"-synctex=1",
+				"-interaction=nonstopmode",
+			},
+		}
 	end,
 }
